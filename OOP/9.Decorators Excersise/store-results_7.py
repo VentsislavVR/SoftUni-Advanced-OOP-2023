2008f3627d7050b6cccd5d0a267d882bf9a14ae0
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-08-14 00:16:30 +0300

    more problems

--- OOP/9.Decorators Excersise/store-results_7.py
+++ OOP/9.Decorators Excersise/store-results_7.py
@@ -1,14 +1,23 @@
 class store_results:
-    def __init__(self,func):
+    def __init__(self, func):
         self.func = func
 
     _file_name = "result.txt"
 
     def __call__(self, *args, **kwargs):
-        with open(store_results._file_name,"a") as result_file:
-            result_file.write(f"Function {self.func.__name__} was called. Result:{self.func(*args)}")
+        with open(store_results._file_name, "a") as result_file:
+            result_file.write(f"Function {self.func.__name__} was called. Result:{self.func(*args)}\n")
 
 
 @store_results
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
+
+
+@store_results
+def mult(a, b):
+    return a * b
+
+
+add(2, 2)
+mult(6, 4)
