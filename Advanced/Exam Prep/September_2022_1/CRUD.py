e4506cbb7f31907274ca5be2e89cdad1c26f4281
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-08-18 00:18:04 +0300

    more problems

--- Advanced/Exam Prep/September_2022_1/CRUD.py
+++ Advanced/Exam Prep/September_2022_1/CRUD.py
@@ -1,15 +1,52 @@
+def Create(direction, value):
+    ...
+
+
+def Update(direction, value):
+    ...
+
+
+def Delete(direction):
+    ...
+
+
+def Read(direction):
+    ...
+
+def check_valid_position(row,col):
+    ...
+
+
 SIZE = 6
 
 matrix = [[x for x in input().split()] for x in range(SIZE)]
 
-
-starting_positions = []
-
+starting_positions = tuple(input())
 
 directions = {
     "up": (-1,0),
     "down": (1,0),
     "left": (0,-1),
     "right": (0,1),
-
 }
+
+while True:
+    command = input()
+    if command == "Stop":
+        break
+                    # i dont get 3 values everytime
+    action,direction,value = command.split()
+
+    if action == "Create":
+        Create(direction,value)
+    elif action == "Update":
+        Update(direction,value)
+    elif action == "Delete":
+        Delete(direction)
+    elif action == "Read":
+        Read(direction)
+
+
+
+
+
