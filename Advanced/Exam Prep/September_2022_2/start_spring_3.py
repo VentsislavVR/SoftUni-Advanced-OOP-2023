1e98508471cbc4de770c3ac494bdb8b7d58b9d7e
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-08-18 11:31:12 +0300

    more problems

---
+++ Advanced/Exam Prep/September_2022_2/start_spring_3.py
@@ -0,0 +1,64 @@
+def start_spring(**kwargs):
+    result = {}
+    for obj,type in kwargs.items():
+        if type not in result.keys():
+            result[type] = []
+        result[type].append(obj)
+
+    sorting = sorted(result.items(),key=lambda x:(-len(x[1]),[0],x[0]))
+    output = ""
+    for key,val in sorting:
+        output += f"{key}:\n"
+        for v in sorted(val):
+            output += f"-{v}\n"
+
+    return output[:-1]
+
+
+example_objects = {"Magnolia": "tree",
+
+"Swallow": "bird",
+
+"Thrushes": "bird",
+
+"Pear": "tree",
+
+"Cherries": "tree",
+
+"Shrikes": "bird",
+
+"Butterfly": "insect"}
+
+print(start_spring(**example_objects))
+
+
+
+
+import unittest
+
+class Tests(unittest.TestCase):
+    def test(self):
+        result = start_spring(
+            **{"Magnolia": "tree",
+                   "Swallow": "bird",
+                   "Thrushes": "bird",
+                   "Pear": "tree",
+                   "Cherries": "tree",
+                   "Shrikes": "bird",
+                   "Butterfly": "insect"}
+
+
+        )
+        self.assertEqual(result.strip(), "bird:\n"
+                                         "-Shrikes\n"
+                                         "-Swallow\n"
+                                         "-Thrushes\n"
+                                         "tree:\n"
+                                         "-Cherries\n"
+                                         "-Magnolia\n"
+                                         "-Pear\n"
+                                         "insect:\n"
+                                         "-Butterfly")
+
+if __name__ == "__main__":
+    unittest.main()
