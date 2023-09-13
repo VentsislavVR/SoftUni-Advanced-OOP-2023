62c1d7ca6bf595ccbced58f7addf4b63d465381d
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-09-13 14:45:30 +0300

    ..

--- Advanced/0.Advanced May 2023/1.Lists asS tacks and Ques Lab/4.Water Dispenser Lab Ines.py
+++ Advanced/0.Advanced May 2023/1.Lists asS tacks and Ques Lab/4.Water Dispenser Lab Ines.py
@@ -1,9 +1,34 @@
 from collections import deque
-people = deque()
+water_line = deque()
 
-water_quantity = int(input())
+quantity = int(input())
 
 name = input()
+while name != 'Start':
+    water_line.append(name)
+    name = input()
+command = input()
+
+while command !='End':
+    data = command.split()
+
+    if len(data) == 1:
+        liters_to_give = int(data[0])
+        person_name = water_line.popleft()
+        if liters_to_give <= quantity:
+
+             print(f"{person_name} got water")
+             quantity -= liters_to_give
+        else:
+            print(f"{person_name} must wait")
+    else:
+        liters_to_add = int(data[1])
+        quantity+=liters_to_add
+
+    command = input()
+
+print(f'{quantity} liters left')
+
 
 
 
