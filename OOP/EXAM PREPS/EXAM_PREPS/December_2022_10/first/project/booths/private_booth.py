6e7c9e09d4dc20c8cb7860e04b2e80a14b974e1b
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-11-30 17:18:24 +0200

    exam prep

---
+++ OOP/EXAM PREPS/EXAM_PREPS/December_2022_10/first/project/booths/private_booth.py
@@ -0,0 +1,13 @@
+from project.booths.booth import Booth
+
+
+class PrivateBooth(Booth):
+
+    @property
+    def get_price_per_person(self):
+        return 3.50
+
+    def reserve(self, number_of_people: int):
+        # if self.capacity >= number_of_people:
+        self.price_for_reservation = self.get_price_per_person * number_of_people
+        self.is_reserved = True
