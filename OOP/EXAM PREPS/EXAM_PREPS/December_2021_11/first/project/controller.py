b47f9f72145510df002114684c851a8ba5328ea1
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-12-07 16:52:31 +0200

    added logick / create methods

--- OOP/EXAM PREPS/EXAM_PREPS/December_2021_11/first/project/controller.py
+++ OOP/EXAM PREPS/EXAM_PREPS/December_2021_11/first/project/controller.py
@@ -1,29 +1,50 @@
 from typing import List
 
 from project.car.car import Car
+from project.core.car_factory import CarFactory
 from project.driver import Driver
 from project.race import Race
 
 
 class Controller:
     def __init__(self):
-        self.cars:List[Car] = []
-        self.drivers:List[Driver] = []
-        self.races:List[Race] = []
+        self.cars: List[Car] = []
+        self.drivers: List[Driver] = []
+        self.races: List[Race] = []
 
-    def create_car(self,car_type: str, model: str, speed_limit: int):
-        ...
-    def create_driver(self,driver_name: str):
+        self.car_factory = CarFactory()
+
+    def create_car(self, car_type: str, model: str, speed_limit: int):
+        if any(c.model == model for c in self.cars):
+            raise Exception(f"Car {model} is already created!")
+
+        try:
+            car = self.car_factory.create_car(
+                car_type, model, speed_limit)
+            self.cars.append(car)
+            return f"{car.__class__.__name__} {car.model} is created."
+        except RuntimeError:
+            pass
+
+    def create_driver(self, driver_name: str):
+        if any(d.name == driver_name for d in self.drivers):
+            raise Exception(f"Driver {driver_name} is already created!")
+        driver = Driver(driver_name)
+        self.drivers.append(driver)
+        return f"Driver {driver_name} is created."
+
+
+    def create_race(self, race_name: str):
+        if any(r.name == race_name for r  in self.races):
+            raise Exception(f"Race {race_name} is already created!")
+        race = Race(race_name)
+        self.races.append(race)
+        return f"Race {race_name} is created."
+    def add_car_to_driver(self, driver_name: str, car_type: str):
         ...
 
-    def create_race(self,race_name: str):
-        ...
-    def add_car_to_driver(self,driver_name: str, car_type: str):
-
-        ...
-    def add_driver_to_race(self,race_name: str, driver_name: str):
-
+    def add_driver_to_race(self, race_name: str, driver_name: str):
         ...
 
-    def start_race(self,race_name: str):
+    def start_race(self, race_name: str):
         ...
---
+++ OOP/EXAM PREPS/EXAM_PREPS/December_2021_11/first/project/main.py
@@ -0,0 +1,7 @@
+from project.controller import Controller
+
+controller = Controller()
+print(controller.create_car('SportsCar','Porsha',550))
+print(controller.create_car('SportsCar','Porsh',600))
+print(controller.create_driver('Gosho'))
+print(controller.create_race('Batovo'))
