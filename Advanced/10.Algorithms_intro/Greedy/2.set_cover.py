c780cce3fb69a71fd5c98a3fb8485854e2f7b5ce
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2024-02-20 16:08:43 +0200

    ..

---
+++ Advanced/10.Algorithms_intro/Greedy/2.set_cover.py
@@ -0,0 +1,23 @@
+def set_cover(universe, sets):
+    chosen_sets = []
+
+    while universe:
+        best_set = max(sets, key=lambda s: len(universe.intersection(s)))
+        chosen_sets.append(best_set)
+        universe -= best_set
+
+    return chosen_sets
+
+
+universe = {x for x in input().split(', ')}
+
+num_sets = int(input())
+
+sets = [set(input().split(', ')) for _ in range(num_sets)]
+result = set_cover(universe,sets)
+
+for i in range(len(result)):
+    result[i] = sorted(result[i])
+
+print(f"Sets to take {len(result)}")
+[print("{ " + f"{', '.join(s)}" + " }") for s in result]
